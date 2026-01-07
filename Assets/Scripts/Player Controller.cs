24bbc802d54d3579a1eb1c9dc386fb3e56632d86
HaydenKuechler <haydenkue.business@gmail.com>
2026-01-07 11:52:23 -0500

    added movement

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,16 +1,44 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControllerr : MonoBehaviour
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerStats stats;
+    //Component references
+
+    private Rigidbody2D rbody;
+
+    //Field references
+
+    private Vector2 moveInput;
+
+    //Start is called before the first frame update
+    void Awake()
     {
-        
+        // Initiallize here 
+        rbody = GetComponent<Rigidbody2D>();
+
+        stats= new PlayerStats();
+        int speed = stats.MoveSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue value)
     {
-        
+        moveInput = value.Get<Vector2>();
     }
+
+   void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    void ApplyMovement ()
+    {
+        float velocityx = moveInput.x;
+
+        rbody.linearVelocity = new Vector2(velocityx , rbody.linearVelocity.y);
+    }
+
 }
---
+++ Assets/Scripts/Player Stats.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerStats
+{
+    private int movespeed;
+    public int MoveSpeed
+    {
+        get
+        { 
+            return movespeed; 
+        }
+        set
+        {
+            movespeed = value;
+        }
+    }
+}
